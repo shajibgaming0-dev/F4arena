42936e4696a514008b28c33e23bb99a2ca8dfcea
shajibgaming0-dev <shajibgaming0@gmail.com>
2026-01-15 21:35:59 +0600

    Add config.js (Admin password + Firebase placeholders)
    Added:
    - ADMIN_PASSWORD
    - Firebase config placeholders
    - Payment number placeholders (Bkash / Nagad)

---
+++ config.js
@@ -0,0 +1,13 @@
+// Admin password
+const ADMIN_PASSWORD = "1234";
+
+// Firebase / Payment placeholders
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  databaseURL: "https://YOUR_DB_URL.firebaseio.com",
+  projectId: "YOUR_PROJECT_ID"
+};
+
+const BKASH_NUMBER = "01XXXXXXXXX";
+const NAGAD_NUMBER = "01XXXXXXXXX";
